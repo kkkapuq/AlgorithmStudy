41329d9f7d1a9a532d9945a65f8493b9c71a1515
HongKiTea <82351890+HongKiTea@users.noreply.github.com>
2022-04-27 01:29:27 +0900

    Add files via upload

---
+++ Python/220426_BOJ_1620_나는야 포켓몬 마스터 이다솜/Hong.py
@@ -0,0 +1,17 @@
+import sys
+
+n, m = map(int, input().split())
+
+dicName = {}
+dicNum = {}
+for i in range(1, n + 1):
+    name = sys.stdin.readline.strip()
+    dicName[i] = name
+    dicNum[name] = i
+
+for i in range(m):
+    answer = sys.stdin.readline.strip()
+    if answer.isdigit():
+        print(dicName[int(answer)])
+    else:
+        print(dicNum[answer])

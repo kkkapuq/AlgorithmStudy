9b18fbcb5bd7641ae8d99d0ba1067755d9ffe264
HongKiTea <82351890+HongKiTea@users.noreply.github.com>
2022-04-28 19:40:07 +0900

    Add files via upload

---
+++ Python/220427_BOJ_1629_곱셈/Hongkitae.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+input = stdin.readline
+
+a, b, c = map(int, input().split())
+
+# 모듈러 곱셈법칙 응용
+#   a*b % c == (a % c * b % c) % c
+
+def modular_def(a, b, c):
+    if b == 1:
+        return a % c
+    temp = modular_def(a, int(b/2), c)
+    
+    # 지수법칙 응용 a^7 = a^3 * a^3 * a
+    # 지수가 짝수
+    if num2 % 2 == 0:
+        return temp * temp % denominator
+    # 홀수
+    else:
+        return temp * temp * num1 % denominator
+        
+print(modular_def(a, b, c))

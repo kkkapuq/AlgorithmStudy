5b8c9e3fa2a5929fbff03aea8bb3136eb2288655
HyungJoonCho <fhrek@naver.com>
2022-05-28 16:24:44 +0900

    노트북 로컬에 있던거 푸시

--- Python/220508_BOJ_9935_문자열 폭발/HyungJoonCho.py
+++ Python/220508_BOJ_9935_문자열 폭발/HyungJoonCho.py
@@ -6,6 +6,7 @@ str = list(sys.stdin.readline().strip())
 
 # 폭발 문자열 입력받기
 bombStr = list(sys.stdin.readline().strip())
+tempStr = "".join(bombStr)
 
 # 폭발 문자열 길이
 bombLen = len(bombStr)
@@ -14,7 +15,7 @@ bombLen = len(bombStr)
 stack = []
 
 # 잘못된 풀이
-# # 일반문자열 크기만큼 for문 돌면서, 폭발문자열과 같은 문자라면 pop해주기
+# 일반문자열 크기만큼 for문 돌면서, 폭발문자열과 같은 문자라면 pop해주기
 # for i in str:
 #     stack.append(i)
 #     if i in bombStr:
@@ -29,8 +30,7 @@ stack = []
 for i in str:
     stack.append(i)
     if i == bombStr[-1]:
-        tempStr = "".join(bombStr)
-        if tempStr == "".join(stack[-bombLen:]):
+        if bombStr == (stack[-bombLen:]):
             del(stack[-bombLen:])
 
 if stack:

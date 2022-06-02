34b77d5b390da4682bd4141ce130f0849a40e035
HyungjoonCho <fhrek@naver.com>
2022-06-03 00:44:26 +0900

    11399 풀이 - 너무 쉬워서 딱히 설명할게없음;

---
+++ Python/220602_BOJ_11399_ATM/HyungJoonCho.py
@@ -0,0 +1,18 @@
+n = int(input())
+list = list(map(int, input().split()))
+'''
+일찍 돈을 뽑는 사람이 앞에 있을 수록 시간이 단축된다.
+1. 리스트 오름차순으로 정렬
+2. 정렬된 리스트 for문 돌면서 출력하면 끝인듯?
+3. 시간복잡도는 nlogn? 
+'''
+
+list.sort()
+cnt = 0
+sum = 0
+
+for i in list:
+    cnt += i
+    sum += cnt
+    
+print(sum)

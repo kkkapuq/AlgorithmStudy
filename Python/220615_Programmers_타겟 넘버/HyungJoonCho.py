d5a1b1777bda1b2b71a5c2309235a172bdc69d90
HyungJoonCho <fhrek@naver.com>
2022-06-16 11:32:05 +0900

    타겟넘버 풀이 - 호출된 값을 토대로 +하거나 - 해서 target이 나올때까지 재귀를 돌려주는게 핵심 - 근데 이게 왜 DFS/BFS인지는 이해가 잘 안된다...

--- Python/220615_Programmers_타겟 넘버/HyungJoonCho.py
+++ Python/220615_Programmers_타겟 넘버/HyungJoonCho.py
@@ -1,5 +1,30 @@
+'''
+1. 
+'''
+
+from collections import deque
+import sys
+
+sys.setrecursionlimit(10000)
+
+answer = 0
+
+def dfs(numLen, i, result, numbers, target):
+    if i == numLen:
+        if result == target:
+            global answer
+            answer += 1
+        return
+    dfs(numLen, i + 1, result + numbers[i], numbers, target)
+    dfs(numLen, i + 1, result - numbers[i], numbers, target)
+
+
 def solution(numbers, target):
-    answer = 0
+    global answer
     
+    numLen = len(numbers)
+    dfs(numLen, 0, 0, numbers, target)
     
     return answer
+
+solution([1, 1, 1, 1, 1,], 3)

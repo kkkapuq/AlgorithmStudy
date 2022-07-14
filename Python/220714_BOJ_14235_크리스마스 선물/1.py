c51275a9b7982a90fe6ad16966d090fc61872311
HyungJoonCho <fhrek@naver.com>
2022-07-14 14:54:24 +0900

    14235 풀이 - 우선순위 큐를 활용했다. - 우선순위 큐는 이제 어느정도 활용법을 아는 것 같음. 개념 파악 잘 하고 어떤 때 써야되는지 빠르게 캐치하자

---
+++ Python/220714_BOJ_14235_크리스마스 선물/1.py
@@ -0,0 +1,22 @@
+'''
+1. 우선순위 큐를 사용한다.
+2. 가치가 가장 큰 선물을 줘야되니까, heappush할 때 -1 곱해서 넣어주고, heappop할 때 -1 곱해서 원상복구 시켜준다 (최대힙)
+'''
+
+import heapq
+import sys
+
+n = int(input())
+
+q = []
+
+for i in range(n):
+    tempList = list(map(int, sys.stdin.readline().split()))
+    if tempList[0] == 0:
+        if len(q) < 1:
+            print(-1)
+        else:
+            print(-1*heapq.heappop(q))
+    else:
+        for j in range(1, len(tempList)):
+            heapq.heappush(q, -1*tempList[j])

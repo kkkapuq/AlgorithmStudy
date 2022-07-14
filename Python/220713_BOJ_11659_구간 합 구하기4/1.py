a8f10c4e89ec15ddac2fa4448017b460795f51af
HyungJoonCho <fhrek@naver.com>
2022-07-14 09:38:31 +0900

    11659 수정 - input() 으로 하면 시간초과나서 sys로 수정

--- Python/220713_BOJ_11659_구간 합 구하기4/1.py
+++ Python/220713_BOJ_11659_구간 합 구하기4/1.py
@@ -2,10 +2,11 @@
 1. 그냥 풀면 시간초과 난다. 누적합으로 풀자
 2. sumList에 1번째부터 n번째까지의 합을 넣어주고, 마지막엔 sumList[j] - sumList[i-1] 로 출력만해주면됨.
 '''
+import sys
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
 
-numList = list(map(int, input().split()))
+numList = list(map(int, sys.stdin.readline().split()))
 sumList = [0]
 
 tempSum = 0
@@ -15,5 +16,5 @@ for i in numList:
     sumList.append(tempSum)
 
 for _ in range(m):
-    i, j = map(int, input().split())
+    i, j = map(int, sys.stdin.readline().split())
     print(sumList[j] - sumList[i-1])

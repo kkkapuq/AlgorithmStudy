d0f706ce7b5c468bd6d99f3fad35c09beefc451d
HyungjoonCho <fhrek@naver.com>
2022-07-29 23:46:32 +0900

    18870 풀이 - 좌표압축이라는 개념을 아는데 시간이 오래걸렸음. - 딕셔너리를 사용해 시간초과 탈출할 것. (O(1))

---
+++ Python/220728_BOJ_18870_좌표 압축/1.py
@@ -0,0 +1,19 @@
+'''
+1. 좌표압축 = 작은순서부터 인덱싱 해주는것
+2. 입력된 좌표 리스트를 정렬해준다음... 순서대로 딕셔너리에 넣어주면 될듯?
+'''
+n = int(input())
+
+# 입력하고 정렬
+numList = list(map(int, input().split()))
+
+# set으로 중복 제거
+setList = list(set(numList))
+setList.sort()
+answerDic = {}
+
+for i in range(len(setList)):
+    answerDic[setList[i]] = i
+
+for i in range(len(numList)):
+    print(answerDic[numList[i]], end=' ') 
